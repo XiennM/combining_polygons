5ebbebf8bd372cdc8cdc78567d415916ffe53e48
Ksenia M <kmochalova@sfedu.ru>
2024-10-30 02:55:59 +0300

    done

--- combinig_polygons/Form1.Designer.cs
+++ combinig_polygons/Form1.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -58,17 +61,52 @@
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(493, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Найти объединение";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 8);
+            this.textBox1.MaxLength = 50;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(475, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 30);
+            this.textBox2.MaxLength = 50;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(475, 20);
+            this.textBox2.TabIndex = 5;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(877, 527);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -76,6 +114,9 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- combinig_polygons/Form1.cs
+++ combinig_polygons/Form1.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Reflection;
 
 namespace combinig_polygons
 {
@@ -16,6 +19,8 @@ namespace combinig_polygons
 
         List<List<Point>> _polygons;
         List<Point> _points;
+        List<Tuple<Point, bool>> _pointsp1_intersect = new List<Tuple<Point, bool>>();
+        List<Tuple<Point, bool>> _pointsp2_intersect = new List<Tuple<Point, bool>>();
 
         Bitmap _bm;
         Graphics _g;
@@ -83,6 +88,247 @@ namespace combinig_polygons
             _bm = new Bitmap(_size.Width, _size.Height);
             pictureBox1.Image = _bm;
             _g = Graphics.FromImage(pictureBox1.Image);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            intersectPoints();
+            Combine();
+        }
+
+        private void intersectPoints()
+        {
+            _pointsp1_intersect = new List<Tuple<Point, bool>>();
+            _pointsp2_intersect = new List<Tuple<Point, bool>>();
+            List<Point> _pointsp1 = _polygons[0];
+            List<Point> _pointsp2 = _polygons[1];
+
+            Point a;
+            Point b;
+            Point c;
+            Point d;
+            Point n;
+            float t;
+            List<Point> res = new List<Point>();
+
+            for (int i = 0; i <= _pointsp1.Count - 1; i++)
+            {
+                _pointsp1_intersect.Add(new Tuple<Point, bool>(_pointsp1[i], false));
+                for (int j = 0; j <= _pointsp2.Count - 1; j++)
+                {
+                    a = _pointsp1[i];
+                    b = _pointsp1[(i + 1) % _pointsp1.Count];
+                    c = _pointsp2[j];
+                    d = _pointsp2[(j + 1) % _pointsp2.Count];
+                    n = new Point(-(d.Y - c.Y), d.X - c.X);
+
+                    t = -(float)(n.X * (a.X - c.X) + n.Y * (a.Y - c.Y)) / (n.X * (b.X - a.X) + n.Y * (b.Y - a.Y));
+
+
+                    res.Add(new Point(a.X + (int)(t * (b.X - a.X)), a.Y + (int)(t * (b.Y - a.Y))));
+                    if (res[res.Count - 1].X >= Math.Min(a.X, b.X) && res[res.Count - 1].X <= Math.Max(a.X, b.X) && res[res.Count - 1].Y >= Math.Min(a.Y, b.Y) && res[res.Count - 1].Y <= Math.Max(a.Y, b.Y)
+                        && res[res.Count - 1].X >= Math.Min(c.X, d.X) && res[res.Count - 1].X <= Math.Max(c.X, d.X) && res[res.Count - 1].Y >= Math.Min(c.Y, d.Y) && res[res.Count - 1].Y <= Math.Max(c.Y, d.Y))
+                    {
+                        _g.DrawRectangle(new Pen(Color.Red), res[res.Count - 1].X, res[res.Count - 1].Y, 3, 3);
+                        pictureBox1.Image = _bm;
+                        _pointsp1_intersect.Add(new Tuple<Point, bool>(res[res.Count - 1], true));
+                    }
+                }
+            }
+
+            for (int i = 0; i < _pointsp1_intersect.Count - 1; i++)
+            {
+                // Находим две точки с флагом IsIntersect == true
+                if (_pointsp1_intersect[i].Item2 == true && _pointsp1_intersect[(i + 1) % _pointsp1_intersect.Count].Item2 == true)
+                {
+                    Point temp1 = _pointsp1_intersect[(i - 1) % _pointsp1_intersect.Count].Item1;
+                    Point temp2 = _pointsp1_intersect[(i + 2) % _pointsp1_intersect.Count].Item1;
+
+                    // Извлекаем точки с IsIntersect == true между ними
+                    var innerPoints = _pointsp1_intersect.GetRange(i, 2)
+                        .OrderBy(p => dist(temp1.X, temp1.Y, p.Item1.X, p.Item1.Y)) // Сортировка по расстоянию к a
+                        .ToList();
+
+                    // Заменяем исходные точки на отсортированные
+                    _pointsp1_intersect[i] = innerPoints[0];
+                    _pointsp1_intersect[i + 1] = innerPoints[1];
+                }
+            }
+
+
+            res = new List<Point>();
+
+
+            for (int i = 0; i <= _pointsp2.Count - 1; i++)
+            {
+                _pointsp2_intersect.Add(new Tuple<Point, bool>(_pointsp2[i], false));
+                for (int j = 0; j <= _pointsp1.Count - 1; j++)
+                {
+                    a = _pointsp1[j];
+                    b = _pointsp1[(j + 1) % _pointsp1.Count];
+                    c = _pointsp2[i];
+                    d = _pointsp2[(i + 1) % _pointsp2.Count];
+                    n = new Point(-(d.Y - c.Y), d.X - c.X);
+
+                    t = -(float)(n.X * (a.X - c.X) + n.Y * (a.Y - c.Y)) / (n.X * (b.X - a.X) + n.Y * (b.Y - a.Y));
+
+
+                    res.Add(new Point(a.X + (int)(t * (b.X - a.X)), a.Y + (int)(t * (b.Y - a.Y))));
+                    if (res[res.Count - 1].X >= Math.Min(a.X, b.X) && res[res.Count - 1].X <= Math.Max(a.X, b.X) && res[res.Count - 1].Y >= Math.Min(a.Y, b.Y) && res[res.Count - 1].Y <= Math.Max(a.Y, b.Y)
+                        && res[res.Count - 1].X >= Math.Min(c.X, d.X) && res[res.Count - 1].X <= Math.Max(c.X, d.X) && res[res.Count - 1].Y >= Math.Min(c.Y, d.Y) && res[res.Count - 1].Y <= Math.Max(c.Y, d.Y))
+                    {
+                        _g.DrawRectangle(new Pen(Color.Red), res[res.Count - 1].X, res[res.Count - 1].Y, 3, 3);
+                        pictureBox1.Image = _bm;
+                        _pointsp2_intersect.Add(new Tuple<Point, bool>(res[res.Count - 1], true));
+                    }
+                }
+
+            }
+
+
+            for (int i = 0; i < _pointsp2_intersect.Count - 1; i++)
+            {
+                // Находим две точки с флагом IsIntersect == true
+                if (_pointsp2_intersect[i].Item2 == true && _pointsp2_intersect[(i + 1) % _pointsp2_intersect.Count].Item2 == true)
+                {
+                    Point temp1 = _pointsp2_intersect[(i - 1) % _pointsp2_intersect.Count].Item1;
+                    Point temp2 = _pointsp2_intersect[(i + 2) % _pointsp1_intersect.Count].Item1;
+
+                    // Извлекаем точки с IsIntersect == true между ними
+                    var innerPoints = _pointsp2_intersect.GetRange(i, 2)
+                        .OrderBy(p => dist(temp1.X, temp1.Y, p.Item1.X, p.Item1.Y)) // Сортировка по расстоянию к a
+                        .ToList();
+
+                    // Заменяем исходные точки на отсортированные
+                    _pointsp2_intersect[i] = innerPoints[0];
+                    _pointsp2_intersect[i + 1] = innerPoints[1];
+                }
+            }
+
+
+            /*
+            textBox1.Text = "1: " + _pointsp1_intersect.Count + " 2: " + _pointsp2_intersect.Count;
+            int k = 0;
+            foreach (Tuple<Point, bool> point in _pointsp2_intersect)
+            {
+                if (_pointsp1_intersect.FindIndex(x => x.Equals(point)) != -1)
+                    k++;
+            }
+            textBox1.Text += "OOO: " + k;*/
+            foreach (Tuple<Point, bool> point in _pointsp2_intersect)
+            {
+                //if (point.Item2 == true)
+                    textBox2.Text += "  " + point.Item1.X + "," + point.Item1.Y;
+                if (point.Item2 == true)
+                    textBox2.Text += "!";
+
+            }
+            foreach (Tuple<Point, bool> point in _pointsp1_intersect)
+            {
+                //if (point.Item2 == true)
+                    textBox1.Text += "  " + point.Item1.X + "," + point.Item1.Y;
+                if (point.Item2 == true)
+                    textBox1.Text += "!";
+
+            }
+        }
+
+        private double dist(int x1, int x2, int y1, int y2)
+        {
+            return Math.Sqrt(((x2 - x1)^2 + (y2 - y1)^2));
+        }
+
+        private void Combine()
+        {
+            Point lowest_p = _pointsp1_intersect[0].Item1;
+            bool inFirst = true;
+
+            for (int i = 0; i <= _pointsp1_intersect.Count - 1; i++)
+            {
+                if (lowest_p.Y < _pointsp1_intersect[i].Item1.Y) {
+                    lowest_p = _pointsp1_intersect[i].Item1;
+                }
+            }
+
+            for (int i = 0; i <= _pointsp2_intersect.Count - 1; i++)
+            {
+                if (lowest_p.Y < _pointsp2_intersect[i].Item1.Y)
+                {
+                    lowest_p = _pointsp2_intersect[i].Item1;
+                    inFirst = false;
+                }
+            }
+
+            List<Point> res = new List<Point>();
+            res.Add(lowest_p);
+
+            if(inFirst)
+            {
+                int k = _pointsp1_intersect.FindIndex(x => x.Item1 == lowest_p);
+                k = (k + 1) % _pointsp1_intersect.Count;
+                while (_pointsp1_intersect[k].Item1 != lowest_p)
+                {
+                    while (_pointsp1_intersect[k].Item2 != true)
+                    {
+                        res.Add(_pointsp1_intersect[k].Item1);
+                        k = (k + 1) % _pointsp1_intersect.Count;
+                        if (_pointsp1_intersect[k].Item1 == lowest_p)
+                            break;
+                    }
+                    if (_pointsp1_intersect[k].Item1 == lowest_p)
+                        break;
+                    k = _pointsp2_intersect.FindIndex(x => x.Item1.Equals(_pointsp1_intersect[k].Item1));
+                    res.Add(_pointsp2_intersect[k].Item1);
+                    k = (k + 1) % _pointsp2_intersect.Count;
+                    while (_pointsp2_intersect[k].Item2 != true)
+                    {
+                        res.Add(_pointsp2_intersect[k].Item1);
+                        k = (k + 1) % _pointsp2_intersect.Count;
+                    }
+                    k = _pointsp1_intersect.FindIndex(x => x.Item1.Equals(_pointsp2_intersect[k].Item1));
+                    res.Add(_pointsp1_intersect[k].Item1);
+                    k = (k + 1) % _pointsp1_intersect.Count;
+                }
+            }
+
+            if (!inFirst)
+            {
+                int k = _pointsp2_intersect.FindIndex(x => x.Item1 == lowest_p);
+                k = (k + 1) % _pointsp2_intersect.Count;
+                while (_pointsp2_intersect[k].Item1 != lowest_p)
+                {
+                    while (_pointsp2_intersect[k].Item2 != true)
+                    {
+                        res.Add(_pointsp2_intersect[k].Item1);
+                        k = (k + 1) % _pointsp2_intersect.Count;
+                        if (_pointsp2_intersect[k].Item1 == lowest_p)
+                            break;
+                    }
+                    if (_pointsp2_intersect[k].Item1 == lowest_p)
+                        break;
+                    k = _pointsp1_intersect.FindIndex(x => x.Item1.Equals(_pointsp2_intersect[k].Item1));
+                    res.Add(_pointsp1_intersect[k].Item1);
+                    k = (k + 1) % _pointsp1_intersect.Count;
+                    while (_pointsp1_intersect[k].Item2 != true)
+                    {
+                        res.Add(_pointsp1_intersect[k].Item1);
+                        k = (k + 1) % _pointsp1_intersect.Count;
+                    }
+                    k = _pointsp2_intersect.FindIndex(x => x.Item1.Equals(_pointsp1_intersect[k].Item1));
+                    res.Add(_pointsp2_intersect[k].Item1);
+                    k = (k + 1) % _pointsp2_intersect.Count;
+                }
+            }
+
+
+            Pen pen = new Pen(Color.Red);
+
+            List<Point> polygon = res;
+                for (int i = 1; i < polygon.Count; i++)
+                {
+                    _g.DrawLine(pen, polygon[i - 1].X, polygon[i - 1].Y, polygon[i].X, polygon[i].Y);
+                }
+                _g.DrawLine(pen, polygon[0].X, polygon[0].Y, polygon[polygon.Count - 1].X, polygon[polygon.Count - 1].Y);
         }
     }
 }
